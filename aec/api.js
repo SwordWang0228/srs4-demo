e77db9fa8d59a6974c1184bdce83b9b2676138ac
gavin <chenzhexiang@outlook.com>
2022-05-08 23:59:13 +0800

    add interface

--- aec/api.js
+++ aec/api.js
@@ -7,4 +7,28 @@ function freeAecInstance(aecmHandler)
    return Module._freeAecInstance(aecmHandler);
 }
 
+function initializeAecInstance(aecmHandler,sampFreq){
+
+   return Module._initializeAecInstance(aecmHandler,sampFreq);
+}
+
+function bufferFarend(aecmHandler, farend, nrOfSamples) {
+
+   return Module._bufferFarend(aecmHandler, farend, nrOfSamples);
+}
+
+
+function aecmProcess(aecmHandler, nearendNoisy, nearenNoisyLen , nearendClean, nrOfSamples, msInSndCardBuf) {
+   return Module._aecmProcess(aecmHandler, nearendNoisy, nearenNoisyLen , nearendClean, nrOfSamples, msInSndCardBuf);
+}
+
+
+function setConfig(aecmHandler, echoMode, cngMode) {
+   return Module._setConfig(aecmHandler, echoMode, cngMode);
+}
+
+function malloc(size) {
+   return Module._malloc(size);
+}
+
 libaes = Module;
