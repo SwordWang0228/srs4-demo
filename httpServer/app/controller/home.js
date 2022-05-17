0209907c1505fdcfbe7e8acd44a47ad586bec598
Liu Ji <mynameisliuji@foxmail.com>
2022-05-17 22:48:36 +0800

    httpCallback

--- httpServer/app/controller/home.js
+++ httpServer/app/controller/home.js
@@ -7,6 +7,17 @@ class HomeController extends Controller {
     const { ctx } = this;
     ctx.redirect('/public/one2oneAudio.html');
   }
+
+  async httpCallback() {
+    const { ctx, app } = this;
+    const { logger } = app;
+    const body = ctx.request.body;
+    logger.info('[home.js] srs httpCallback body: ', body);
+    // 成功: srs客户端可以继续 操作
+    this.ctx.body = 0;
+    // 失败: 中断srs客户端操作
+    this.ctx.body = 1;
+  }
 }
 
 module.exports = HomeController;
